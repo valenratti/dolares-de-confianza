4a322bac39f0663085a5a79f604d8eda2ded4f4b
Seba Itokazu <sebitokazu@gmail.com>
2021-12-14 00:36:26 -0300

    Add spring-boot-starter-web, .gitignore & application.properties

--- src/main/java/ar/edu/itba/infocracks/bd2/dolaresdeconfianza/DolaresDeConfianzaApplication.java
+++ src/main/java/ar/edu/itba/infocracks/bd2/dolaresdeconfianza/DolaresDeConfianzaApplication.java
@@ -1,13 +1,18 @@
 package ar.edu.itba.infocracks.bd2.dolaresdeconfianza;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DolaresDeConfianzaApplication {
+public class DolaresDeConfianzaApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(DolaresDeConfianzaApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }

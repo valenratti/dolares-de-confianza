0287b36fb6895830a2f3055b0baf2f820df3c903
lgalende <lgalende@itba.edu.ar>
2021-12-22 00:03:00 -0300

    Modificacion query OfferRepository

--- src/main/java/ar/edu/itba/infocracks/bd2/dolaresdeconfianza/repository/postgres/OfferRepository.java
+++ src/main/java/ar/edu/itba/infocracks/bd2/dolaresdeconfianza/repository/postgres/OfferRepository.java
@@ -10,9 +10,10 @@ import java.util.List;
 @Repository
 public interface OfferRepository extends JpaRepository<Offer,Long> {
     @Query(value =
-            "SELECT * FROM offers WHERE from_currency LIKE ?1 AND to_currency LIKE ?2 " +
-                    "AND min_rate <= ?3  AND max_rate >= ?4 AND min_amount <= ?5 AND max_amount >= ?6 " +
-                    "ST_DWithin(cast(users.location as geography),ST_SetSRID(ST_Point(?7, ?8), 4326), ?9)", nativeQuery = true)
+            "SELECT * FROM offers o WHERE o.from_currency LIKE ?1 AND o.to_currency LIKE ?2 " +
+                    "AND o.min_rate <= ?3  AND o.max_rate >= ?4 AND o.min_amount <= ?5 AND o.max_amount >= ?6 " +
+                    "ST_DWithin(cast((SELECT DISTINCT u.location FROM users u WHERE u.id = o.user) as geography), " +
+                    "ST_SetSRID(ST_Point(?7, ?8), 4326), ?9)", nativeQuery = true)
     List<Offer> findOffersByCurrencyAndRateAndAmountAndDistance(
             String fromCurrency, String toCurrency, double minRate, double maxRate,
             double minAmount, double maxAmount, double locationX, double locationY, Double distance);
